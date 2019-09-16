2188797351aba8ff6302322e09c3252ab688e335
Rennan Francisco Messias de Lima <mrennan.lima@gmail.com>
2019-09-16 10:37:30 -0500

    Persiste a inscrição

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -1,6 +1,8 @@
 from django.core import mail
 from django.test import TestCase
+
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 
 
 class SubscribeGetTest(TestCase):
@@ -51,6 +53,9 @@ class SubscribePostValidTest(TestCase):
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
 
+    def test_save_subscription(self):
+        self.assertTrue(Subscription.objects.exists())
+
 
 class SubscribePostInvalidTest(TestCase):
     def setUp(self):
@@ -70,6 +75,9 @@ class SubscribePostInvalidTest(TestCase):
     def test_form_has_errors(self):
         form = self.resp.context['form']
         self.assertTrue(form.errors)
+
+    def test_dont_save_subscription(self):
+        self.assertFalse(Subscription.objects.exists())
 
 
 class SubscribeSuccessMessageTest(TestCase):
