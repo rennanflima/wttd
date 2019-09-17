94612bd621257c96dd1a15b2fa50c2f52ff3d36b
Rennan Francisco Messias de Lima <mrennan.lima@gmail.com>
2019-09-17 10:09:37 -0500

    Padroniza nomes

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -19,6 +19,11 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(cpf='12345')
         self.assertFormErrorCode(form, 'cpf', 'length')
 
+    def test_name_must_be_capitalized(self):
+        form = self.make_validated_form(name='RENNAN lima')
+        self.assertEqual('Rennan Lima', form.cleaned_data['name'])
+
+
     def assertFormErrorCode(self, form, field, code):
         errors = form.errors.as_data()
         errors_list = errors[field]
