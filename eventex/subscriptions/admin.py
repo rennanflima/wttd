bddbc00f2ff2350c6d7af45cdb584c3930ceb986
Rennan Francisco Messias de Lima <mrennan.lima@gmail.com>
2019-09-17 09:19:35 -0500

    Marca como pago varios

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -21,7 +21,14 @@ class SubscriptionModelAdmin(admin.ModelAdmin):
     subscribed_today.boolean = True
 
     def mark_as_paid(self, request, queryset):
-        queryset.update(paid=True)
+        count = queryset.update(paid=True)
+
+        if count == 1:
+            msg = '{} inscrição foi marcada como paga.' 
+        else:
+            msg = '{} inscrições foram marcadas como pagas.'
+
+        self.message_user(request, msg.format(count))
 
     mark_as_paid.short_description = 'Marcar como pago'
 
