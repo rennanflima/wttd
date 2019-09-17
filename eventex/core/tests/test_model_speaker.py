6986ecbbc27593d9028f0d646e606d54f8cce059
Rennan Francisco Messias de Lima <mrennan.lima@gmail.com>
2019-09-17 17:27:07 -0500

    Iniciado o cadastro dos palestrantes

---
+++ eventex/core/tests/test_model_speaker.py
@@ -0,0 +1,25 @@
+from django.shortcuts import resolve_url as r
+from django.test import TestCase
+
+from eventex.core.models import Speaker
+
+
+class SpeakerModelTest(TestCase):
+    def test_create(self):
+        speaker = Speaker.objects.create(
+                name='Grace Hopper',
+                slug='grace-hopper',
+                photo='http://hbn.link/hopper-pic',
+                website='http://hbn.link/hopper-site',
+                description='Programadora e almirante.'
+        )
+
+        self.assertTrue(Speaker.objects.exists())
+
+    def test_description_can_be_blank(self):
+        field = Speaker._meta.get_field('description')
+        self.assertTrue(field.blank)
+    
+    def test_website_can_be_blank(self):
+        field = Speaker._meta.get_field('website')
+        self.assertTrue(field.blank)
