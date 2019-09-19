ad9674028b8e3cf92fac03105592a37c1f897edf
Rennan Francisco Messias de Lima <mrennan.lima@gmail.com>
2019-09-19 10:47:12 -0500

    fix warning pep8

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -2,10 +2,10 @@ from django.views.generic import DetailView, ListView
 
 from eventex.core.models import Course, Speaker, Talk
 
+
 home = ListView.as_view(template_name='index.html',
                         model=Speaker)
 
 speaker_detail = DetailView.as_view(model=Speaker)
-
 
 talk_list = ListView.as_view(model=Talk)
